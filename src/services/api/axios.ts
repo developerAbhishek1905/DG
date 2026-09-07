535a57686d8aab568f80e4b51a893002ff4380c1
developerAbhishek1905 <developer.abhishek1905@gmail.com>
2026-09-07 09:58:23 +0530

    url 1

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: import.meta.env.VITE_API_URL,
 
-  // baseURL:"https://dg-backend-grva.onrender.com/api/v1"
+  baseURL:"https://dg-backend-grva.onrender.com/api/v1"
   // headers: {
   //   "Content-Type": "application/json",
   // },
